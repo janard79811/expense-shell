afa07cd805f1e68050ca8e6ac41568603f325f47
Adarsh <adhiadarsh97@gmail.com>
2023-11-12 19:29:59 +0530

    declared a variable i.e. log_file=/tmp/expense.log, color="\e[33m" & used that variable in script instead of /tmp/expense.log, e[33m  To avoid using multiple times /tmp/expense.log & "e[33m"

--- frontend.sh
+++ frontend.sh
@@ -5,19 +5,19 @@ color="\e[33m"
 echo -e "${color} Installing Ngninx \e[0m"
 dnf install nginx -y &>>${log_file}
 
-echo -e "\${color} copying expense config file \e[0m"
+echo -e "${color} copying expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>${log_file}
 
-echo -e "\${color} removing the default content in Nginx i.e. clean old Nginx content \e[0m"
+echo -e "${color} removing the default content in Nginx i.e. clean old Nginx content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-echo -e "\${color} downloading frontend application code \e[0m"
+echo -e "${color} downloading frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-echo -e "\${color} Extracting/ unzipping downloaded application content \e[0m"
+echo -e "${color} Extracting/ unzipping downloaded application content \e[0m"
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\${color} Starting Nginx service \e[0m"
+echo -e "${color} Starting Nginx service \e[0m"
 systemctl enable nginx &>>${log_file}
 systemctl restart nginx &>>${log_file}
