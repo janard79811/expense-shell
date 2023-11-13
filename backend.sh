a00de4189331e2e3d73c8674f3b48854a7e34960
Adarsh <adhiadarsh97@gmail.com>
2023-11-13 14:33:14 +0530

    Update if condition to print success/ failure instead of 0/1 to 255

--- backend.sh
+++ backend.sh
@@ -19,46 +19,91 @@ fi
 
 echo -e "${color} install nodejs \e[0m"
 dnf install nodejs -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} copying backend service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} add application user \e[0m"
 useradd expense &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} create application directory \e[0m"
 mkdir /app &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} removing/cleaning/ deleting default/old application content \e[0m"
 rm -rf /app/* &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} extract application content \e[0m"
 cd /app &>>${log_file}
 unzip /tmp/backend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} download nodejs dependencies \e[0m"
 npm install &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} install mysql client to load schema \e[0m"
 dnf install mysql -y &>>${log_file}
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} load schema \e[0m"
 mysql -h mysql-dev.janard4589.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} starting backend service \e[0m"
 systemctl daemon-reload &>>${log_file}
 systemctl enable backend &>>${log_file}
 systemctl start backend &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
