1f4d701ade56ad7e9af7a69fd7a61a588281d957
Adarsh <adhiadarsh97@gmail.com>
2023-11-13 13:30:39 +0530

    Update if condition to print success/ failure instead of 0/1 to 255

--- backend.sh
+++ backend.sh
@@ -11,7 +11,11 @@ fi
 
 echo -e "${color} enable nodejs 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+ echo -e "\e[32m SUCCESS \e[0m"
+else
+ echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "${color} install nodejs \e[0m"
 dnf install nodejs -y &>>${log_file}
