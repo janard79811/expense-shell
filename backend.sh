89f5fc5882ea531e253c506d41fb7cbc6b1bf3f5
Adarsh <adhiadarsh97@gmail.com>
2023-11-13 15:13:00 +0530

    Provide "id expense" with if condition to add user when the username doesnot exist only.

--- backend.sh
+++ backend.sh
@@ -33,12 +33,15 @@ else
  echo -e "\e[31m FAILURE \e[0m"
 fi
 
-echo -e "${color} add application user \e[0m"
-useradd expense &>>${log_file}
-if [ $? -eq 0 ]; then
- echo -e "\e[32m SUCCESS \e[0m"
-else
- echo -e "\e[31m FAILURE \e[0m"
+id expense &>>${log_file}
+if [ $? -ne 0 ]; then
+  echo -e "${color} add application user \e[0m"
+  useradd expense &>>${log_file}
+  if [ $? -eq 0 ]; then
+   echo -e "\e[32m SUCCESS \e[0m"
+  else
+   echo -e "\e[31m FAILURE \e[0m"
+  fi
 fi
 
 echo -e "${color} create application directory \e[0m"
