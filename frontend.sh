4b9a36331e0497be94d32ae0651dcddb3801201f
Adarsh <adhiadarsh97@gmail.com>
2023-11-14 12:53:42 +0530

    Updated frontend.sh, mysql.sh with source i.e. common.sh file which is because i.e. common for all scripts and also updated mysql.sh with inputs from backend.sh for password validation steps.

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,27 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
 
 echo -e "${color} Installing Ngninx \e[0m"
 dnf install nginx -y &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} copying expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} removing the default content in Nginx i.e. clean old Nginx content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} downloading frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} Extracting/ unzipping downloaded application content \e[0m"
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} Starting Nginx service \e[0m"
 systemctl enable nginx &>>${log_file}
 systemctl restart nginx &>>${log_file}
-echo $?
+status_check
--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,29 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
+
+MYSQL_ROOT_PASSWORD=$1
+
+if [ -z "$1" ]; then
+  echo password is missing
+  exit
+fi
 
 echo -e "${color} disable mysql default version \e[0m"
 dnf module disable mysql -y &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} copying mysql repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} install mysql server \e[0m"
 dnf install mysql-community-server -y &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} start mysql server \e[0m"
 systemctl enable mysqld &>>${log_file}
 systemctl start mysqld &>>${log_file}
-echo $?
+status_check
 
 echo -e "${color} set mysql password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>${log_file}
-echo $?
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>${log_file}
+status_check
